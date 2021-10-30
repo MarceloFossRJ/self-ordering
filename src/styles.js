8c04a878d80d0e1831d06c91bee2144b216a38d2
Marcelo Foss <marcelo.foss.rj@gmail.com>
2021-10-30 20:21:09 -0300

    first commit

---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import { makeStyles } from "@material-ui/core";
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      red: {
+        backgroundColor: '#ff2040',
+        color: '#ffffff',
+      },
+      main: {
+        flex: 1,
+        overflow: 'auto',
+        flexDirection: 'column',
+        display: 'flex',
+        color: '#ffffff',
+      },
+      center: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+      },
+      black: {
+        backgroundColor: '#000',
+      },
+      largeLogo: {
+        height: 100,
+      },
+}))
